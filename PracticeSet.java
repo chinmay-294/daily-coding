772641867f48c71bb8486ee6e233851a3d50ba95
Chinmay Khairnar <chinmay.khairnar.756@gmail.com>
2025-06-19 00:12:20 +0530

    Add files via upload

---
+++ PracticeSet.java
@@ -0,0 +1,28 @@
+package chinmay.com;
+import java.util.Scanner;
+public class PracticeSet {
+    public static void main(String[] args) {
+//        prctice1==1
+        System.out.println("Sum Of Three numbers");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter num1");
+        float a = sc.nextInt();
+        System.out.println("enter num2");
+        float b = sc.nextInt();
+        System.out.println("enter num3");
+        float c = sc.nextInt();
+
+        float sum = a+b+c;
+        System.out.println(sum);
+
+        System.out.println("Enter Your Name:");
+        Scanner sc= new Scanner(System.in);
+        String name = sc.next();
+        System.out.println("Hello " + name + " have a good day");
+
+
+
+
+    }
+}
