c3b53155d6561e3fc01170ecd4f5108c4f6607c1
Chinmay Khairnar <chinmay.khairnar.756@gmail.com>
2025-06-20 00:53:42 +0530

    Add files via upload

---
+++ String_Prac.java
@@ -0,0 +1,11 @@
+package chinmay.com;
+
+public class String_Prac {
+    public static void main(String[] args) {
+        String name = "Chinmay";
+        System.out.println(name);
+
+        String a = new String("Chinmay");
+        System.out.println(a);
+    }
+}
