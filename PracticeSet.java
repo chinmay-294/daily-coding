8125483a634b4ae0575ddb9bc46d6ed27847e1bd
Chinmay Khairnar <chinmay.khairnar.756@gmail.com>
2025-06-20 00:33:18 +0530

    Add files via upload

--- PracticeSet.java
+++ PracticeSet.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class PracticeSet {
     public static void main(String[] args) {
 //        prctice1==1
-        System.out.println("Sum Of Three numbers");
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("enter num1");
-        float a = sc.nextInt();
-        System.out.println("enter num2");
-        float b = sc.nextInt();
-        System.out.println("enter num3");
-        float c = sc.nextInt();
+//        System.out.println("Sum Of Three numbers");
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.println("enter num1");
+//        float a = sc.nextInt();
+//        System.out.println("enter num2");
+//        float b = sc.nextInt();
+//        System.out.println("enter num3");
+//        float c = sc.nextInt();
 
         float sum = a+b+c;
         System.out.println(sum);
