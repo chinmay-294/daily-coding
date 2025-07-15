16f29fbff1466f85c0d551df2fcadd8eda6cc8a2
Chinmay Khairnar <chinmay.khairnar.756@gmail.com>
2025-07-15 14:11:51 +0530

    Add files via upload

---
+++ StaticDemo.java
@@ -0,0 +1,11 @@
+package Fuel.com;
+
+public class StaticDemo {
+    static int a = 100;            // Static variable with a value
+    static String name = "Java";   // Another static variable
+
+    public static void addition(int x, int y) {  // Static method
+        int sum = x + y;
+        System.out.println("Sum is: " + sum);
+    }
+}
