130608c970cb12d0f6db79a5c35cd86c5767bd44
Chinmay Khairnar <chinmay.khairnar.756@gmail.com>
2025-07-16 23:09:55 +0530

    Add files via upload

---
+++ Type_Casting.java
@@ -0,0 +1,21 @@
+package Fuel.com;
+
+public class Type_Casting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		Implicit (Automatic) : small->big
+//		Explict (Manual) : big-> small
+	
+		int x = 10;
+		int y = x;
+		System.out.println(y);
+		
+		double d = 65.5;
+		int z = (int)d;
+		System.out.println(z);
+		
+	}
+
+}
